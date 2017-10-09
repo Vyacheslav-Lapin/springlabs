11c14498ab71cc2d4144d968c9411b1b2f82e028
Vyacheslav-Lapin <09vUev1GPSNH4v0e>
2017-10-09 17:23:22 +0300

    JDBC - CP

---
+++ src/test/java/lab/jdbc/cp/ConnectionPoolTest.java
@@ -0,0 +1,34 @@
+package lab.jdbc.cp;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.function.Supplier;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+class ConnectionPoolTest {
+
+    @Test
+    void get() throws SQLException {
+        Supplier<Connection> connectionSupplier = new ConnectionPool("/init.sql");
+        //language=SQL
+        String sql = "SELECT id, first_name, last_name, permission, dob, email, password, address, telephone FROM Person";
+
+        int count = 0;
+        try (Connection connection = connectionSupplier.get();
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(sql)) {
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("first_name"));
+                count++;
+            }
+        }
+
+        assertThat(count, is(4));
+    }
+}
